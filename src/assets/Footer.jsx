9bfdc14caa2dd67da55668af6d2032b2ad2ea0f5
Arpan Mandal <72576079+mandalarpan99@users.noreply.github.com>
2026-02-05 20:02:37 +0530

    Update Footer.jsx
    change of footer year.

--- src/assets/Footer.jsx
+++ src/assets/Footer.jsx
@@ -12,7 +12,7 @@ export const Footer = ()=>{
                     <div className="footer_content">
                         <Row>
                             <Col xl={6} md={6} xs={5} className='footer-name'>
-                                <p>Arpan Mandal &copy; 2025. All rights reserved.</p>
+                                <p>Arpan Mandal &copy; 2026. All rights reserved.</p>
                             </Col>
                             <Col xl={3} md={3} xs={3} className='footer-love'>
                                 <p>Made with <span className='love_symble'>&#10084;</span></p>
